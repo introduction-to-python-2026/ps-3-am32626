e485b5c6c2d69369761cb11fd355c54199c08e4a
am32626 <ayalamaman00@gmail.com>
2025-11-11 19:21:28 +0200

    Refactor move function to handle edge cases
    Fix logic for moving '1' in the list and handle edge cases.

--- edge_case_exercise.py
+++ edge_case_exercise.py
@@ -1,9 +1,18 @@
 def move(my_list, direction):
+   if index_of_one == 0 and direction == "left"
+return my_list
+   elif index of one == len(my_list) - 1 and direction == "right"
+return my_list
 
-    # Finds the index of the one in the list
-    index_of_one = my_list.index(1)
 
-    # Move the one to the left or to the right
+def move(my_list, direction): 
+  index_of_one == my_list.index(1)
+  if direction == "left":
+   move[index_of_one] = my_list[index_of_one - 1]
+  elif direction == "right":
+    move[index_of_one] = my_list[index_of_one + 1]
+    
+
     if direction == 'right':
         my_list[index_of_one] = 0
         my_list[index_of_one + 1] = 1
