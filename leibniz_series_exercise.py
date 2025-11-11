972ef9a350e05d1b1801eebe07d34872814e720c
am32626 <ayalamaman00@gmail.com>
2025-11-11 20:10:28 +0200

    Add function to approximate Pi using Leibniz series
    Implement the Leibniz series to approximate Pi.

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,2 +1,14 @@
 def approximate_pi(n_terms):
-    pass # replace pass with your code
+  total_sum = 0
+  denominator = 1
+  sign = 1
+ for i in range(n_terms):
+     corrent_term =  sign / denominator
+     total_sum = total_sum + correct_term
+     denominator = denominator + 2
+     sign = sign * -1
+     result = total_sum * 4
+  return result
+    
+
+
