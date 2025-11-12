cfc6657c6c09c45456b4faf12230b84e11d2bdef
am32626 <ayalamaman00@gmail.com>
2025-11-12 14:13:39 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,23 +1,13 @@
 def approximate_pi(n_terms):
+  total_sum = 0
+  denominator = 1
+  sign = 1
+  for i in range(n_terms):
+    corrent_terms = sign / denominator
+    total_sum += corrent_terms
+    denominator += 2
+    sign *= -1
+  return total_sum * 4
 
-    pass # replace pass with your code
-
-    total_sum = 0
-
-    denominator = 1
-
-    sign = 1
-    n_terms = 10
-    for i in range(n_terms):
-
-    correct_term = sign / denominator
-
-    total_sum = total_sum + correct_turm
-
-    denominator = denominator + 2
-
-    sign = sign * -1
-
-    result = total_sum * 4
-
-return result
+print(approximate_pi(1000000))
+    
