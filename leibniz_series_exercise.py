c362a63da03b7f422b1deaeff910ba640122fca9
am32626 <ayalamaman00@gmail.com>
2025-11-12 12:38:27 +0200

    Correct variable name in Leibniz series function
    Fix variable name typo in Leibniz series calculation.

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -3,13 +3,13 @@ def approximate_pi(n_terms):
   total_sum = 0
   denominator = 1
   sign = 1
- for i in range(n_terms):
-     corrent_term =  sign / denominator
-     total_sum = total_sum + correct_term
-     denominator = denominator + 2
-     sign = sign * -1
-     result = total_sum * 4
-  return result
+for i in range(n_terms):
+    correct_term = sign / denominator
+    total_sum = total_sum + correct_turm
+    denominator = denominator + 2
+    sign = sign * -1
+    result = total_sum * 4
+return result
     
 
 
